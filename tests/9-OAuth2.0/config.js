dffdf9d1137a2193f221f84a5725a52cdf13d273
ionatibia <iker.onatibia@gmail.com>
2016-03-21 19:01:56 +0100

    oauth2Google ok

---
+++ tests/9-OAuth2.0/config.js
@@ -0,0 +1,15 @@
+var config = {
+	google:{
+		client:{
+			id: '865654658487-g3563mv9u10e61l4v72s60tunbkt1rf3.apps.googleusercontent.com',
+			secret: 'fnSmMf3aI18hUXuHm7KaxNKq',
+			callbackUrl:'http://localhost:3000/auth/google/callback'
+		}
+	},
+
+	session:{
+		secret:'secreto'
+	}
+}
+
+module.exports = config;
