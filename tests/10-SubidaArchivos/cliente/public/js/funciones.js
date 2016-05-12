f8e99155f685fb3056e9153da4ae0d412fb8b863
ionatibia <iker.onatibia@gmail.com>
2016-05-12 12:00:57 +0200

    subida archivos ok

--- tests/10-SubidaArchivos/cliente/public/js/funciones.js
+++ tests/10-SubidaArchivos/cliente/public/js/funciones.js
@@ -1,29 +1,4 @@
 $(document).ready(function() {
+	
 
-
-	/*$('#enviar').click(function(event) {
-		console.log("submit");
-		event.preventDefault();
-		var data = $('#formularioFotos').serializeArray();
-		console.log(data);
-		$.ajax({
-			url: 'http://localhost:3000/guardarFoto',
-			type: 'post',
-			dataType: 'json',
-			data: data,
-			success: function(data) {
-				console.log(data);
-			}
-		})
-		.done(function() {
-			console.log("success");
-		})
-		.fail(function() {
-			console.log("error");
-		})
-		.always(function() {
-			console.log("complete");
-		});
-		
-	});*/
 });
